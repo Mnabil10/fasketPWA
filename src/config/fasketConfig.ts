55c4c276e7599cf9fb318772cb96af6f0349544f
Mnabil10 <nabilm604@gmail.com>
2026-01-17 13:41:35 +0200

    add new feature

--- src/config/fasketConfig.ts
+++ src/config/fasketConfig.ts
@@ -3,7 +3,7 @@ export const FASKET_CONFIG = {
   cityCoverage: "All districts of Badr City, Cairo",
   workingHours: "Daily from 9:00 AM to 1:00 AM",
   websiteUrl: "https://fasket.shop",
-  webAppUrl: "https://fasket.shop/app",
+  webAppUrl: "https://fasket.shop",
   supportEmail: "info@fasket.com",
   supportPhone: "+201233329707",
   whatsappNumber: "+201233329707",

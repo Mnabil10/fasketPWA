53c2c62057cf387fecd44f7ecfda25e8e355e20e
Mostafa Alazhariy <87443208+MAlazhariy@users.noreply.github.com>
2026-01-18 14:36:10 +0200

    add network plugin support and update configuration

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/app/',
+  base: process.env.CAPACITOR_BUILD ? './' : '/app/',
   plugins: [
     react(),
     legacy(),

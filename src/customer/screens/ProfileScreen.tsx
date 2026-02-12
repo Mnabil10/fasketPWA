54be61d2314b44cfa73ac43a5b820c7abdb9d723
Mostafa Alazhariy <87443208+MAlazhariy@users.noreply.github.com>
2026-02-12 09:37:04 +0200

    feat(FasketConfig): introduce showDeleteAccountButton for iOS before App Store deadline; update ProfileScreen to utilize new feature

--- src/customer/screens/ProfileScreen.tsx
+++ src/customer/screens/ProfileScreen.tsx
@@ -56,7 +56,7 @@ import { resolveSupportConfig } from "../utils/mobileAppConfig";
 import { logout as logoutApi } from "../../services/auth";
 import { useQueryClient } from "@tanstack/react-query";
 import { useLocalCartStore } from "../stores/localCart";
-import { isAppStoreDeadlinePassed } from "../../config/fasketConfig";
+import { showDeleteAccountButton } from "../../config/fasketConfig";
 import appLogo from "../../../icons/icon-256.webp";
 
 interface ProfileScreenProps {
@@ -709,7 +709,7 @@ export function ProfileScreen({ appState, updateAppState }: ProfileScreenProps) 
                 <LogOut className="w-5 h-5 mr-3" />
                 <span>{t("profile.logout")}</span>
               </Button>
-              {isAppStoreDeadlinePassed && (
+              {showDeleteAccountButton && (
                 <Button
                   variant="outline"
                   onClick={() => setDeleteAccountSheetOpen(true)}
